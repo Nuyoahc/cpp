328b5b07e668f9eb078333a51a7842761c756c41
NuyoahC <2132468793@qq.com>
2025-08-09 22:09:32 +0800

    跳台阶拓展问题

---
+++ training/day33/1.cc
@@ -0,0 +1,13 @@
+// https://www.nowcoder.com/practice/953b74ca5c4d44bb91f39ac4ddea0fee?tpId=230&tqId=39750&ru=/exam/oj?
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n = 0; cin >> n;
+    
+    cout << pow(2, n - 1) << endl;
+
+    return 0;
+}
